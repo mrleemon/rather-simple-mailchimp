a889397cc07c29626a94e990a569bfcbb88f75c8
leemon <tacrapo@gmail.com>
2017-04-10 21:09:16 +0200

    code refactoring

--- my-simple-mailchimp.php
+++ my-simple-mailchimp.php
@@ -8,23 +8,81 @@ Author URI: http://oscarciutat.com/code/
 Description: A simple Mailchimp plugin with a shortcode and a widget
 */
 
-require_once( 'include/my-simple-mailchimp-widget.php' );
-
 class My_Simple_Mailchimp {
-	
-	/*
-	 * constructor
+
+	/**
+	 * Plugin instance.
 	 *
 	 * @since 1.0
+	 *
 	 */
-    function __construct() {
-        add_action( 'init', array($this, 'init') );
-		add_action( 'wp_enqueue_scripts', array($this, 'enqueue_scripts') );
-		add_shortcode( 'mailchimp', array($this, 'shortcode_mailchimp' ) );
+	protected static $instance = null;
+
+
+	/**
+	 * Access this plugin’s working instance
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public static function get_instance() {
+		
+		if ( !self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+
 	}
 
-	function init() {
-		load_plugin_textdomain('my-simple-mailchimp', '', dirname(plugin_basename( __FILE__ )) . '/languages/');
+	
+	/**
+	 * Used for regular plugin work.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public function plugin_setup() {
+
+  		$this->includes();
+
+        add_action( 'init', array( $this, 'load_language' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		
+		add_shortcode( 'mailchimp', array( $this, 'shortcode_mailchimp' ) );
+
+	}
+
+	
+	/**
+	 * Constructor. Intentionally left empty and public.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	public function __construct() {}
+
+	
+	
+ 	/**
+	 * Includes required core files used in admin and on the frontend.
+	 *
+	 * @since 1.0
+	 *
+	 */
+	protected function includes() {
+		require_once( 'include/my-simple-mailchimp-widget.php' );
+	}
+
+	
+	/**
+	 * Loads language
+	 *
+	 * @since 1.0
+	 *
+	 */
+	function load_language() {
+		load_plugin_textdomain( 'my-simple-mailchimp', '', dirname(plugin_basename( __FILE__ ) ) . '/languages/' );
 	}
 
 	
@@ -32,17 +90,17 @@ class My_Simple_Mailchimp {
 	 * enqueue_scripts
 	 */
 	function enqueue_scripts() {
-		wp_enqueue_script( 'mc-validate', plugins_url('/js/mc-validate.js', __FILE__), array('jquery'), '1.0', true );
-		wp_enqueue_script( 'mc-messages', plugins_url('/js/mc-messages.js', __FILE__), array('mc-validate'), '1.0', true );
-		wp_enqueue_style( 'my-simple-mailchimp-css', plugins_url('/style.css', __FILE__) );
+		wp_enqueue_script( 'mc-validate', plugins_url( '/js/mc-validate.js', __FILE__ ), array( 'jquery' ), '1.0', true );
+		wp_enqueue_script( 'mc-messages', plugins_url( '/js/mc-messages.js', __FILE__ ), array( 'mc-validate' ), '1.0', true );
+		wp_enqueue_style( 'my-simple-mailchimp-css', plugins_url( '/style.css', __FILE__ ) );
 	}
 
 
 	/**
 	 * shortcode_mailchimp
 	 */
-	function shortcode_mailchimp($atts) {
-		$html = $this->shortcode_atts($atts);
+	function shortcode_mailchimp( $atts ) {
+		$html = $this->shortcode_atts( $atts );
 		return $html;
 	}
 
@@ -50,23 +108,23 @@ class My_Simple_Mailchimp {
 	/**
 	 * shortcode_atts
 	 */
-	function shortcode_atts($atts) {
-		extract(shortcode_atts(array(
+	function shortcode_atts( $atts ) {
+		extract( shortcode_atts( array(
 			'url' => '',
 			'u' => '',
 			'id' => ''
-		), $atts));
+		), $atts ) );
 		$html = '<!-- Begin MailChimp Signup Form -->
 		<div id="mc_embed_signup">
-			<form action="' . esc_attr(untrailingslashit($atts['url'])) . '/subscribe/post?u=' . esc_attr($atts['u']) . '&amp;id=' . esc_attr($atts['id']) .'" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+			<form action="' . esc_attr( untrailingslashit( $atts['url'] ) ) . '/subscribe/post?u=' . esc_attr( $atts['u'] ) . '&amp;id=' . esc_attr( $atts['id'] ) .'" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			<div id="mc_embed_signup_scroll">
-				<div style="position: absolute; left: -5000px;"><input type="text" name="b_' . esc_attr($atts['u']) . '_' . esc_attr($atts['id']) . '" tabindex="-1" value=""></div>
+				<div style="position: absolute; left: -5000px;"><input type="text" name="b_' . esc_attr( $atts['u'] ) . '_' . esc_attr( $atts['id'] ) . '" tabindex="-1" value=""></div>
 				<div class="mc-field-group">
-					<label for="mce-EMAIL">' . __('email:', 'my-simple-mailchimp') . '<span class="required">*</span></label>
+					<label for="mce-EMAIL">' . __( 'email:', 'my-simple-mailchimp' ) . '<span class="required">*</span></label>
 					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 				</div>
 				<div class="mc-submit-button">
-					<input type="submit" value="' . __('subscribe', 'my-simple-mailchimp') . '" name="subscribe" id="mc-embedded-subscribe" class="button">
+					<input type="submit" value="' . __( 'subscribe', 'my-simple-mailchimp' ) . '" name="subscribe" id="mc-embedded-subscribe" class="button">
 				</div>
 				<div id="mce-responses" class="clear">
 					<div class="response" id="mce-error-response" style="display:none"></div>
@@ -82,4 +140,4 @@ class My_Simple_Mailchimp {
 	
 }
 
-$my_simple_mailchimp = new My_Simple_Mailchimp;
+add_action( 'plugins_loaded', array ( My_Simple_Mailchimp::get_instance(), 'plugin_setup' ) );
