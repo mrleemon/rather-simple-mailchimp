971dd5d0aaf1ad37582adb5e42c50999974cdbf6
leemon <tacrapo@gmail.com>
2020-11-23 08:40:14 +0100

    Use json datatype instead of jsonp Reference classes instead of ids Remove comments

--- assets/js/mc-subscribe.js
+++ assets/js/mc-subscribe.js
@@ -6,20 +6,18 @@
             url: $form.attr( 'action' ),
             data: $form.serialize(),
             cache: false,
-            dataType: 'jsonp',
+            dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             error: function( err ) {
                 console.log( 'error' );
             },
             success: function( data ) {
                 if ( data.result != 'success' ) {
-                    console.log( data.msg );
-                    $( '#mce-error-response', $form ).show();
-                    $( '#mce-error-response', $form ).html( '<p>' + data.msg.substring( 4 ) + '</p>' );
+                    $( '.mce-error-response', $form ).show();
+                    $( '.mce-error-response', $form ).html( '<p>' + data.msg.substring( 4 ) + '</p>' );
                 } else {
-                    console.log( data.msg );
-                    $( '#mce-success-response', $form ).show();
-                    $( '#mce-success-response', $form ).html( '<p>Thank you for subscribing. We have sent you a confirmation email.</p>' );
+                    $( '.mce-success-response', $form ).show();
+                    $( '.mce-success-response', $form ).html( '<p>Thank you for subscribing. We have sent you a confirmation email.</p>' );
                 }
             }
         });
@@ -27,12 +25,9 @@
 
     $( '.mc-embedded-subscribe-form' ).on( 'submit', function( e ) {
         try {
-            // define argument as the current form especially if you have more than one
             var $form = $( this );
-            // stop open of new tab
             e.preventDefault();
-            // submit form via ajax
-            subscribe( $form ) ;
+            subscribe( $form );
         } catch ( error ) {
         }
     });
