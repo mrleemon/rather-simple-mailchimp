0f600611fdc1512a1ff74e1938d66156ca6695f5
leemon <tacrapo@gmail.com>
2022-11-26 17:40:15 +0100

    Add default values to boolean attrs

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9891203925ff6d7a2a27');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ee876bc9a0ae5319bb1e');
