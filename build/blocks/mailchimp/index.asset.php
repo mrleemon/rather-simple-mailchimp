d53b5714d19c69df49747f7d07515c6aa14ca7c4
leemon <tacrapo@gmail.com>
2023-07-23 00:11:37 +0200

    Use custom icon

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'aff252b29c3145a8b46b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ce51aeff91464890774a');
