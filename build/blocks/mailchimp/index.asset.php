734a9237d45928868309aac84abe51547a157f71
leemon <tacrapo@gmail.com>
2023-08-11 08:03:17 +0200

    Update apiVersion to 3

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ce51aeff91464890774a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '04c4b61348a41a7e3a73');
