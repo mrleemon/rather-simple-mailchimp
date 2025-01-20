3ab8418b14e65d617e6299ec2a1456b68ef69a5d
leemon <tacrapo@gmail.com>
2025-01-20 15:15:31 +0100

    Rebuild

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ef71aef8ca6843f6f7aa');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '0835882e1bf2054c4a14');
