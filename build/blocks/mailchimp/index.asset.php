b855dd61c023c3bdf6c5f903f73fca9f6872dbb9
leemon <tacrapo@gmail.com>
2025-02-12 12:32:20 +0100

    Fix styles

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '0835882e1bf2054c4a14');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '18a033b818046e43527f');
