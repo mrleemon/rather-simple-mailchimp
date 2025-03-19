2bd417e3decff2cc3cf08510ff445ff855981764
leemon <tacrapo@gmail.com>
2025-03-19 12:58:05 +0100

    Add DocBlock Simplify code

--- src/blocks/mailchimp/render.php
+++ src/blocks/mailchimp/render.php
@@ -1,8 +1,17 @@
 <?php
-	$block_attributes = get_block_wrapper_attributes()
+/**
+ * All of the parameters passed to the function where this file is being required are accessible in this scope:
+ *
+ * @param array    $attributes     The array of attributes for this block.
+ * @param string   $content        Rendered block output. ie. <InnerBlocks.Content />.
+ * @param WP_Block $block          The instance of the WP_Block class that represents the block being rendered.
+ *
+ * @package rather_simple_mailchimp
+ */
+
 ?>
 <div
-	<?php echo wp_kses_data( $block_attributes ); ?>
+	<?php echo wp_kses_data( get_block_wrapper_attributes() ); ?>
 	<?php
 	echo wp_interactivity_data_wp_context(
 		array(
