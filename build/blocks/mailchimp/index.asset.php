78a850f05eccf4eb9188e411fcc830890f74bf71
leemon <tacrapo@gmail.com>
2026-02-11 18:00:00 +0100

    Rebuild

--- build/blocks/mailchimp/index.asset.php
+++ build/blocks/mailchimp/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '980e81f6daa72c5ec392');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '6fd5c3ee1afadce1c333');
--- build/blocks/mailchimp/view.asset.php
+++ build/blocks/mailchimp/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '4becfdeb52a05b40d590', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'b23b46c51210fb114202', 'type' => 'module');
